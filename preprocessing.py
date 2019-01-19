3b6e0d2cf42d258c2075c5600459fb472d0d4a78
jaysonph <40629085+jaysonph@users.noreply.github.com>
2019-01-19 17:56:54 +0800

    Text cleaning and standardizing

---
+++ preprocessing.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+# Function to convert to lowercase and standardize the format of all punction space
+def clean_txt(train,test):
+    train["question_text"] = train["question_text"].str.lower()
+    test["question_text"] = test["question_text"].str.lower()
+
+    puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£', 
+     '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…', 
+     '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─', 
+     '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞', 
+     '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√', ]
+    def clean_text(x):
+
+        x = str(x)
+        for punct in puncts:
+            x = x.replace(punct, f' {punct} ')
+        return x
+
+
+    train["question_text"] = train["question_text"].apply(lambda x: clean_text(x))
+    test["question_text"] = test["question_text"].apply(lambda x: clean_text(x))
+    return train, test
